02ace961a1632f9775f64c63e1a12ac1cb4266d0
Tery Lim <kumabotz@gmail.com>
2015-03-03 23:57:15 +1300

    Impl

--- learnyounode/7-http-client.js
+++ learnyounode/7-http-client.js
@@ -1,7 +1,9 @@
 var http = require('http');
 
 var url = process.argv[2] || 'http://nodeschool.io/';
-console.log(url);
-http.get(url, function(res) {
-  console.log(res);
+
+http.get(url, function(response) {
+  response.setEncoding('utf-8');
+  response.on('data', console.log);
+  response.on('error', console.error);
 });
